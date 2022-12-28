09e09c8ee32fd808b4c624dd79ddfc18b985be8a
Nutanix Japan <lakshmi.balaramane@nutanix.com>
2022-12-28 15:57:20 +0900

    Wed Dec 28 15:57:20 JST 2022

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,8 @@ const config = {
       ({
         docs: {
           routeBasePath: '/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
