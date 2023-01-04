dcf13f58c58a39210c988ac69d99d8c90b4aa7e6
Nutanix Japan <lakshmi.balaramane@nutanix.com>
2023-01-04 14:06:15 +0900

    Wed Jan  4 14:06:15 JST 2023

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
+  // GitHub pages deployment config
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nutanix-japan', // Usually your GitHub org/user name.
   projectName: 'ocp-gitp', // Usually your repo name.
